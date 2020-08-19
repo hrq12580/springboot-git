42951f3ace8e3110af8cb3389049385a3c544491
hrq12580 <1245038757@qq.com>
2020-08-19 20:51:43 +0800

    添加hello方法

--- src/main/java/com/jk/controller/UserController.java
+++ src/main/java/com/jk/controller/UserController.java
@@ -1,4 +1,11 @@
 package com.jk.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+
 public class UserController {
+
+    @RequestMapping("hello")
+    public void hello(){
+
+    }
 }
